b2fa5523b781bf2bdf1e136d24aa45a457cb184a
gallol <luciano.gallo@gmail.com>
2019-11-09 23:24:01 -0300

    faqs html y php

--- proyecto-integral-master/faqs/faqs.php
+++ proyecto-integral-master/faqs/faqs.php
@@ -2,15 +2,39 @@
 <html lang="en" dir="ltr">
   <head>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
     <meta charset="utf-8">
-    <title></title>
+    <title>Postflix F.A.Q.S.</title>
   </head>
   <body>
     <div class="container">
         <h1>Preguntas Frecuentes</h1>
-          <button type="button" name="button">¿Que es Netflix?</button>
+          <button type="button" name="button-a">¿Que es Postflix?</button>
+          <div class="card-a">
+            <div class="card-body">
+              Postflix es una red que ofrece una gran bla bla bla bla bla
+            </div>
+          </div>
+          <button type="button" name="button-b">¿Como me registro?</button>
+          <div class="card-b">
+            <div class="card-body">
+              Para registrarse en Postflix es muy simple, hace click aqui y completa el registro para poder puntear tus peliculas o series favoritas!
+            </div>
+          </div>
+          <button type="button" name="button-c">¿Como recomiendo una pelicula o serie a un amigo?</button>
+          <div class="card-b">
+            <div class="card-body">
+              Para recomendar o puntuar alguna pelicula o serie es sencillo, hace click aqui y completa el registro para poder puntear tus peliculas o series favoritas!
+            </div>
+          </div>
+          <button type="button" name="button-d">¿ACA VA OTRA PREGUNTA?</button>
+          <div class="card-b">
+            <div class="card-body">
+              Para registrarse en Postflix es muy simple, hace click aqui y completa el registro para poder puntear tus peliculas o series favoritas!
+            </div>
+          </div>
 
-
+          <button type="button" class="btn btn-danger">Postflix</button>
 
     </div>
      <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
