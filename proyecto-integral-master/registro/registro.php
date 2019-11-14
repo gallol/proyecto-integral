72d10c9b8edd248072381d10b2f97a32cc0d93c3
gallol <luciano.gallo@gmail.com>
2019-11-13 22:29:28 -0300

    probando registro

---
+++ proyecto-integral-master/registro/registro.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Registrarse</title>
+  </head>
+  <body>
+    <div class="container">
+      <header>
+
+        <nav class="navbar navbar-light bg-transparent">
+        <!-- LOGO -->
+          <a title="PointFlix" href="home.php">
+            <img class="logo" src="https://github.com/gallol/proyecto-integral/blob/master/proyecto-integral-master/Imagenes/logo.png?raw=true" alt="Postflix">
+          </a>
+
+    </header>
+  </body>
+</html>
