0f665ce7759069816acef3fd6d6ba5820ff27aec
Agustín Noriega <57065873+agustin-noriega@users.noreply.github.com>
2019-11-17 23:31:28 -0300

    agrego de una palabra (Y)

--- proyecto-integral-master/vista-posteos/vistapost.php
+++ proyecto-integral-master/vista-posteos/vistapost.php
@@ -109,7 +109,7 @@
 
       <div class="container-marketing">
 
-        <h1 id="yourfilms">Your Films</h1>
+        <h1 id="yourfilms">Your Favorite Films</h1>
         <div class="row">
           <div class="col-lg-4">
             <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 140x140"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
