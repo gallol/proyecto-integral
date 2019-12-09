af0f4ab0293c0a1fc9780a9d412ea2143a3bb132
GeraHollmann <57095525+GeraHollmann@users.noreply.github.com>
2019-12-09 10:00:16 -0300

    Add files via upload

--- login.php
+++ login.php
@@ -1,28 +1,24 @@
 <?php
-
-$password ="";
 $email = "";
-  if ($_POST) {
-      if (strlen($_POST["password"]) <= 2){
-      echo "La contraseña debe tener minimo 3 caracteres";
+$password = "";
+session_start();
+require_once 'funciones_proyecto.php';
+$arrayDeErrores = "";
+if($_POST) {
+    $arrayDeErrores = validarLogin($_POST);
+    if(count($arrayDeErrores) === 0) {
+        // REGISTRACION DE USUARIO
+        $usuarioFinal = [
+            'email' => $_POST['email'],
+            'password' => password_hash($_POST['password'], PASSWORD_DEFAULT)
+        ];
+        // ENVIAR A LA BASE DE DATOS $usuarioFinal
+        $jsonDeUsuario = json_encode($usuarioFinal);
+        file_put_contents('usuarios.json', $jsonDeUsuario . PHP_EOL, FILE_APPEND);
+        header("Location: vistapost.php");
+        exit;
     }
-    echo "<br>";
-    if (strlen($_POST["email"]) == 0){
-      echo "Debe completar el campo email.!";
-    }else{
-        if (filter_var($_POST["email"], FILTER_VALIDATE_EMAIL) == false) {
-          echo "El mail no tiene el formato correcto";
-        }
-      };
-
-    if ((strlen($_POST["password"]) >= 3) && filter_var($_POST["email"], FILTER_VALIDATE_EMAIL) == true){
-
-    header('Location: C:\xampp\htdocs\proyecto-integral-master\vistapost.php');exit;
-    }
-    $password = $_POST["password"];
-    $email = $_POST["email"];
-  }
-
+}
 
  ?>
 
@@ -65,42 +61,45 @@ $email = "";
     <div class="container">
     <div class="navbar">
       <!-- BARRA DE NAVEGACION -->
-      <form class="form-inline   col-lg-4 col-xs-12">
-        <a class="elementsnavbar" title="Boton Home" href="index.php">
-          <button class="btn-outline-success boton" style="margin:10px" type="button"> Home </button>
+      <form class="form-inline"   "col-lg-4 col-xs-12">
+        <a title="Boton Home" href="index.php">
+          <button class="btn btn-outline colornav" style="margin:10px" id="boton" type="button"> Home </button>
         </a>
 
-        <a class="elementsnavbar" title="Boton Login" href="./login.php">
-          <button class="btn-outline-success boton" style="margin:10px" type="button"> Login </button>
+        <a title="Boton Login" href="./login.php">
+          <button class="btn btn-outline colornav" style="margin:10px" id="boton" type="button"> Login </button>
         </a>
-        <a class="elementsnavbar" title="Boton Registro" href="./registro.php">
-          <button class="btn-outline-success boton" style="margin:10px" type="button"> Registro </button>
+        <a title="Boton Registro" href="./registro.php">
+          <button class="btn btn-outline colornav" style="margin:10px" id="boton" type="button"> Registro </button>
         </a>
-        <a class="elementsnavbar" title="Boton Mis Posts" href="./vistapost.php">
-          <button class="btn-outline-success boton" style="margin:10px" type="button"> MisPosts </button>
+        <a title="Boton Mis Posts" href="./vistapost.php">
+          <button class="btn btn-outline colornav" style="margin:10px" id="boton" type="button"> Mis Posts </button>
         </a>
-        <a class="elementsnavbar" title="Boton Ayuda" href="./faqs.php">
-          <button class="btn-outline-success boton" style="margin:10px" type="button"> Ayuda </button>
+        <a title="Boton Ayuda" href="./faqs.php">
+          <button class="btn btn-outline colornav" style="margin:10px" id="boton" type="button"> Ayuda </button>
         </a>
-        <a class="elementsnavbar" title="Boton Contacto" href="./contacto.php">
-          <button class="btn-outline-success boton" style="margin:10px" type="button"> Contacto </button>
+        <a title="Boton Contacto" href="./contacto.php">
+          <button class="btn btn-outline colornav" style="margin:10px" id="boton" type="button"> Contacto </button>
         </a>
+        </form>
     </div>
     <div class="signin1">
-      <form class="form-signin">
+      <form class="form-signin" method="post" action="">
         <a href="./index.php"><img class="mb-4" src="./Imagenes/logo.png" alt="" width="170" height="72"></a>
     <h1 class="h3 mb-3 font-weight-normal">Completa con tus datos de usuario para ingresar</h1>
-    <input type="email" id="inputEmail" class="form-control" placeholder="Correo electronico" name="email" value="<?=$email?>" required="" autofocus="">
+    <input type="email" id="inputEmail" class="form-control" placeholder="Correo electronico" name="email" value="<?= persistirDato($arrayDeErrores, 'email'); ?>" required="" autofocus="">
+      <small class="text-danger"><?= isset($arrayDeErrores['email']) ? $arrayDeErrores['email'] : "" ?></small>
     <br>
     <input type="password" id="inputPassword" class="form-control" placeholder="Contraseña" name="password" value="<?=$password?>" required="">
+      <small class="text-danger"><?= isset($arrayDeErrores['password']) ? $arrayDeErrores['password'] : "" ?></small>
     <div class="checkbox mb-3">
       <label>
         <input id="minion" type="checkbox" value="remember-me"> <a id="wity">Recuerdame</a>
     </div>
-    <button id="botonn" class="btn btn-lg btn-primary btn-block" type="submit"><a id="signin" href="./vistapost.php">¡Ingresa!</a></button>
-    <p class="mini mt-5 mb-3 text-muted">©2019</p>
-    <a class="footer" href="./registro.php">¿Todavia no te registraste?</a>
-    <a class="footer" href="./contacto.php">¿Necesitas ayuda para recuperar tu contraseña?</a>
+      <button id="botonn" class="btn btn-lg btn-primary btn-block" type="submit"><a id="signin" href="./vistapost.php">¡Ingresa!</a></button>
+        <p class="mini mt-5 mb-3 text-muted">©2019</p>
+        <a class="footer" href="./registro.php">¿Todavia no te registraste?</a>
+        <a class="footer" href="./contacto.php">¿Necesitas ayuda para recuperar tu contraseña?</a>
       </form>
     </div>
 
