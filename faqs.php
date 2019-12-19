c29d2578172e77e75a4372b6de08bec2d4678246
gallol <49046231+gallol@users.noreply.github.com>
2019-12-19 09:07:08 -0300

    Add files via upload

--- faqs.php
+++ faqs.php
@@ -2,7 +2,7 @@
 <html lang="en" dir="ltr">
   <head>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link href="./css/faqs-style.css" rel="stylesheet">
+    <link href="css/faqs.css" rel="stylesheet">
     <meta charset="utf-8">
     <title>Postflix F.A.Q.S.</title>
   </head>
@@ -23,9 +23,7 @@
         <a title="Boton Mis Posts" href="./vistapost.php">
           <button class="btn btn-outline-success" style="margin:10px" id="boton" type="button"> MisPosts </button>
         </a>
-        <a title="Boton Ayuda" href="./faqs.php">
-          <button class="btn btn-outline-success" style="margin:10px" id="boton" type="button"> Ayuda </button>
-        </a>
+
         <a title="Boton Contacto" href="./contacto.php">
           <button class="btn btn-outline-success" style="margin:10px" id="boton" type="button"> Contacto </button>
         </a>
@@ -40,34 +38,38 @@
 
       </header>
         <h1>Preguntas Frecuentes</h1>
-          <button type="button" name="button-a"> <h2>¿Que es Postflix?</h2> </button>
-          <div class="card-a">
-            <div class="card-body">
-              Postflix es una red social que ofrece la posibilidad de recomendar tus series o peliculas favoritas a tus amigos.
-            </div>
-          </div>
-          <button type="button" name="button-b"><h2>¿Como me Registro?</h2></button>
-          <div class="card-b">
-            <div class="card-body">
-              Para registrarse en Postflix es muy simple, hace  <strong> <a href="../registro/registro.php">click aqui</a> </strong> y completa el registro para poder puntear tus peliculas o series favoritas!
-            </div>
-          </div>
-          <button type="button" name="button-c"> <h2>¿Como recomiendo una pelicula o serie a un amigo?</h2> </button>
-          <div class="card-b">
-            <div class="card-body">
-              Para recomendar o puntuar alguna pelicula o serie es sencillo, hace click aqui y completa el registro para poder puntear tus peliculas o series favoritas!
-            </div>
-          </div>
-          <button type="button" name="button-d"> <h2>¿Aca podria ir otra pregunta ?</h2> </button>
-          <div class="card-b">
-            <div class="card-body">
-              Para registrarse en Postflix es muy simple, hace <strong> <a href="../registro/registro.php">click aqui</a> </strong> y completa el registro para poder puntear tus peliculas o series favoritas!
-            </div>
-          </div>
+<div class="btn-group">
+  <button type="button" class="btn btn-danger dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    ¿Que es Postflix?
+  </button>
+  <div class="dropdown-menu p-4 text-muted" style="max-width: 300px;">
+  <p>
+    Postflix es una red social que ofrece la posibilidad de recomendar tus series o peliculas favoritas a tus amigos.
+  </p>
 
+  </div>
+</div>
+<div class="btn-group">
+  <button type="button" class="btn btn-danger dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+  ¿Como me registro?  </button>
+  <div class="dropdown-menu p-4 text-muted" style="max-width: 200px;">
+  <p> Para registrarse en Postflix es muy simple, hace  <strong> <a href="../registro/registro.php">click aqui</a> </strong> y completa el registro para poder puntear tus peliculas o series favoritas!</p>
+</div>
+</div>
+<div class="btn-group">
+  <button type="button" class="btn btn-danger dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+  ¿Como recomiendo una pelicula o serie a un amigo? </button>
+  <div class="dropdown-menu p-4 text-muted" style="max-width: 200px;">
+  <p> Para recomendar o puntuar alguna pelicula o serie es sencillo, hace click aqui y completa el registro para poder puntear tus peliculas o series favoritas!
+</div>
+</div>
+<div class="btn-group">
+  <button type="button" class="btn btn-danger dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+  ¿Como me contacto con algun representante de Postflix ?</button>
+  <div class="dropdown-menu p-4 text-muted" style="max-width: 200px;">
+  <p> Si tenes alguna otra inquietud o algun problema en particular podes contactarte con nosotros debe este <strong> <a href="contacto.php">link</a> </strong></div>
+</div>
 
-
-    </div>
      <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
