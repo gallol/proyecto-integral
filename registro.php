bae695e046d144c5aadbd0f9d132cfa0acbd450f
gallol <49046231+gallol@users.noreply.github.com>
2019-12-23 14:08:13 -0300

    Add files via upload
    registro andando de nuevo

--- registro.php
+++ registro.php
@@ -7,7 +7,16 @@ $arrayDeErrores = "";
 if($_POST) {
     $arrayDeErrores = validarRegistracion($_POST);
     if(count($arrayDeErrores) === 0) {
-        return $this->crearUsuario();
+      //REGISTRO USUARIO
+      $usuarioFinal = [
+        'nombre' => trim($_POST['nombre']),
+        'email' => $_POST['email'],
+        'password' => password_hash($_POST['password'], PASSWORD_DEFAULT)
+      ];
+      //ENVIAR A LA BASE DE DATOS $usuarioFinal
+      $jsonDeUsuario = json_encode($usuarioFinal);
+      file_put_contents('usuarios.json', $jsonDeUsuario . PHP_EOL, FILE_APPEND);
+        //return $this->crearUsuario();
         header("Location: login.php");
         exit;
     }
